79f864bf21a56581ca8e3f6af9ea20b63575f8c3
piaand <pia.m.andersin@gmail.com>
2021-04-14 12:59:20 +0300

    status after exercise 11.9.

--- cypress/integration/pokedex_app.test.js
+++ cypress/integration/pokedex_app.test.js
@@ -5,4 +5,10 @@ describe('Pokedex', function() {
     cy.contains('ivysaur')
     cy.contains('Pokémon and Pokémon character names are trademarks of Nintendo.')
   })
+
+  it('Individual PokemonPage can be opened', function() {
+    cy.visit('http://localhost:5000')
+    cy.contains('ponyta').click()
+    cy.contains('run away')
+  })
 })
